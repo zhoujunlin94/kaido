cf0582390d1d8407b06f1c4e215575c4604e2b7a
zhoujunlin <zhoujunlin.work@outlook.com>
2024-04-22 21:40:07 +0800

    refactor

--- src/main/java/io/github/zhoujunlin94/kaido/KaidoApplication.java
+++ src/main/java/io/github/zhoujunlin94/kaido/KaidoApplication.java
@@ -1,17 +1,14 @@
 package io.github.zhoujunlin94.kaido;
 
-import io.github.zhoujunlin94.meet.tk_mybatis.MeetTKMybatisAutoConfiguration;
 import org.redisson.spring.starter.RedissonAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.context.annotation.Import;
 
 /**
  * @author zhoujunlin
  */
-@Import(MeetTKMybatisAutoConfiguration.class)
 @EnableAspectJAutoProxy(exposeProxy = true)
 @SpringBootApplication(exclude = {
         /*不要redis自动注入  后续使用redis功能需要删除这两行*/
